0515a4d88d309d1668f9eb7082b207242e7a6700
Oleksandr Livyk <109311598+alxndlk@users.noreply.github.com>
2025-06-14 14:34:34 +0200

    fix build

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Wrapper } from "../layout/Wrapper/Wrapper";
-import { Main } from "./Admin/Main";
 import AdminHeader from "../layout/AdminHeader/AdminHeader";
 
 export default function Page() {
   return (
     <Wrapper>
       <AdminHeader />
-      <Main />
     </Wrapper>
   );
 }
