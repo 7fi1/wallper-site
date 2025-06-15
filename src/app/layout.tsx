448a01af676c82d88391b9621606f4c88e157379
Oleksandr Livyk <109311598+alxndlk@users.noreply.github.com>
2025-06-15 15:52:34 +0200

    added quotes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
         var f=d.getElementsByTagName(t)[0];f.parentNode.insertBefore(s,f);
       }(window,document,'script','https://www.redditstatic.com/ads/pixel.js','rdt');
       
-      window.rdt && window.rdt('init', ${REDDIT_PIXEL_ID});
+      window.rdt && window.rdt('init', '${REDDIT_PIXEL_ID}');
       window.rdt && window.rdt('track', 'PageVisit');
     `,
           }}
