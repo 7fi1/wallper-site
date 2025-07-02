95dd77728d178b0c6fa17069234855b88c741110
Oleksandr Livyk <109311598+alxndlk@users.noreply.github.com>
2025-07-02 22:38:44 +0200

    gif fix

--- src/app/components/home-page/Hero/Block/Block.tsx
+++ src/app/components/home-page/Hero/Block/Block.tsx
@@ -58,7 +58,6 @@ export const Block = () => {
               height={1080}
               quality={70}
               alt={`Mod ${index + 1}`}
-              unoptimized
               className={styles.image}
             />
           </motion.div>
